f0a824070901944a765d2fe0d21ca3034d363118
ViAarisKea <viaa0001@stud.ek.dk>
2025-09-05 18:16:54 +0200

    add registration controller

--- go/handlers/registration.go
+++ go/handlers/registration.go
@@ -1,3 +1,14 @@
 package handlers
 
-//controller
+import "net/http"
+
+type RegistrationController struct {
+}
+
+func (rc *RegistrationController) ShowRegistrationPage() {
+
+}
+
+func (rc *RegistrationController) Register(w http.ResponseWriter, r *http.Request) {
+
+}
