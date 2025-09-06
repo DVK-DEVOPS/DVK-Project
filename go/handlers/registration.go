cc503d9a62f6edc09d17a16cf38081345c70edaa
ViAarisKea <viaa0001@stud.ek.dk>
2025-09-06 14:27:58 +0200

    add render template method

--- go/handlers/registration.go
+++ go/handlers/registration.go
@@ -1,12 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 type RegistrationController struct {
 }
 
-func (rc *RegistrationController) ShowRegistrationPage() {
-
+func (rc *RegistrationController) ShowRegistrationPage(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/registration.html")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Template not found", http.StatusNotFound)
+		return
+	}
 }
 
 func (rc *RegistrationController) Register(w http.ResponseWriter, r *http.Request) {
