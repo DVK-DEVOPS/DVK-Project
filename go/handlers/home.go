64376ee78cff0411c4121e57ecf270b028ad286a
DanielJensenKEA <daje0003@stud.kea.dk>
2025-09-10 18:17:46 +0200

    Comment on homehandler testing. Template is not retrieved

--- go/handlers/home.go
+++ go/handlers/home.go
@@ -6,7 +6,7 @@ import (
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("go/templates/register.html")
+	tmpl, _ := template.ParseFiles("go/templates/register.html") //Removing go/ from path serves the html correctly and does not crash the app when navigating to /
 	err := tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, "Template not found", http.StatusNotFound)
