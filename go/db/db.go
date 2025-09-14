c9b3f25c9770618ce8043261618791813084a787
DanielJensenKEA <daje0003@stud.kea.dk>
2025-09-14 17:55:29 +0200

    Pathing for InitDB changed from testing path to original path

--- go/db/db.go
+++ go/db/db.go
@@ -13,7 +13,7 @@ func InitDB() (*sql.DB, error) {
 		return nil, err
 	}
 
-	sql, err := os.ReadFile("C:/Users/danie/VS Code Projects/DVK-Project/go/schema.sql") //C:\Users\danie\VS Code Projects\DVK-Project\go\schema.sql
+	sql, err := os.ReadFile("./schema.sql") //C:\Users\danie\VS Code Projects\DVK-Project\go\schema.sql
 	if err != nil {
 		return nil, err
 	}
