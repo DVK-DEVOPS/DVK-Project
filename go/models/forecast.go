c1d12a1f9160b60bc8e5cc0e736c9b73cab19deb
ViAarisKea <viaa0001@stud.ek.dk>
2025-09-22 22:10:50 +0200

    add forecast struct

---
+++ go/models/forecast.go
@@ -0,0 +1,12 @@
+package models
+
+type Forecast struct {
+	List []struct {
+		Main struct {
+			Temp float64 `json:"temp"`
+		} `json:"main"`
+		Weather []struct {
+			Description string `json:"description"`
+		} `json:"weather"`
+	} `json:"list"`
+}
