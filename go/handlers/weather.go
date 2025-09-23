fc1525dea2fa51f58d544a5fb828a5d2ae488fd6
ViAarisKea <viaa0001@stud.ek.dk>
2025-09-23 08:33:45 +0200

    add the base structure for weather handler

---
+++ go/handlers/weather.go
@@ -0,0 +1,14 @@
+package handlers
+
+import "net/http"
+
+type WeatherController struct {
+}
+
+func (wc *WeatherController) ShowWeatherPage(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (wc *WeatherController) GetWeatherForecast(w http.ResponseWriter, req *http.Request) {
+
+}
