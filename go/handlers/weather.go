98a490591687d7fd6bc5875db36918a06cfc7b55
ViAarisKea <viaa0001@stud.ek.dk>
2025-09-23 08:39:11 +0200

    add body to show weather page and the template

--- go/handlers/weather.go
+++ go/handlers/weather.go
@@ -1,12 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 type WeatherController struct {
 }
 
 func (wc *WeatherController) ShowWeatherPage(w http.ResponseWriter, req *http.Request) {
-
+	tmpl, _ := template.ParseFiles("templates/weather.html")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Template not found", http.StatusNotFound)
+		return
+	}
 }
 
 func (wc *WeatherController) GetWeatherForecast(w http.ResponseWriter, req *http.Request) {
