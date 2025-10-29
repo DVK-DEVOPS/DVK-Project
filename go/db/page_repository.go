2dfbc963d241d424b8c53cbf96c05356f230a468
ViAarisKea <viaa0001@stud.ek.dk>
2025-10-29 13:54:53 +0100

    update search with parsing time

--- go/db/page_repository.go
+++ go/db/page_repository.go
@@ -35,7 +35,8 @@ func (r *PageRepository) FindSearchResults(searchStr string, language string) ([
 	for rows.Next() {
 		var res Result
 		var lang sql.NullString
-		if err := rows.Scan(&res.Title, &res.Url, &res.Content, &lang, &res.CreatedAt, &res.UpdatedAt); err != nil {
+		var createdAtStr, updatedAtStr string
+		if err := rows.Scan(&res.Title, &res.Url, &res.Content, &lang, &createdAtStr, &updatedAtStr); err != nil {
 			return nil, err
 		}
 
@@ -44,6 +45,8 @@ func (r *PageRepository) FindSearchResults(searchStr string, language string) ([
 		} else {
 			res.Language = ""
 		}
+		res.CreatedAt, _ = time.Parse("2006-01-02 15:04:05", createdAtStr)
+		res.UpdatedAt, _ = time.Parse("2006-01-02 15:04:05", updatedAtStr)
 		results = append(results, res)
 	}
 
