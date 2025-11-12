69f2843714cfe0e43e78d83e1980fc46ca0ba3ea
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-11-12 21:24:51 +0100

    added postgress and set env in example env

--- go/db/db.go
+++ go/db/db.go
@@ -11,26 +11,31 @@ import (
 )
 
 func InitDB() (*sql.DB, error) {
+	_ = godotenv.Load() // loads .env if present
 
-	log.Println("[InitDB] Loading environment variables...")
-	_ = godotenv.Load()
+	dbURL := ""
+	if os.Getenv("APP_ENV") == "dev" {
+		log.Println("[InitDB] Using local dev DB URL...")
+		dbURL = os.Getenv("DB_URL")
+	} else {
+		log.Println("[InitDB] Using production DB URL from Key Vault...")
+		dbURL = config.GetSecret(
+			"DB_URL",
+			os.Getenv("AZURE_KEYVAULT_NAME"),
+			os.Getenv("DB_URL_SECRET_NAME"),
+		)
+	}
 
-	log.Println("[InitDB] Getting database URL secret...")
-	dbURL := config.GetSecret("DB_URL", os.Getenv("AZURE_KEYVAULT_NAME"), os.Getenv("DB_URL_SECRET_NAME"))
-
-	log.Println("[InitDB] Opening database connection...")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
-		log.Printf("[InitDB] Error opening database connection: %v\n", err)
 		return nil, err
 	}
 
-	log.Println("[InitDB] Pinging database to verify connection...")
 	if err := db.Ping(); err != nil {
-		log.Printf("[InitDB] Error pinging database: %v\n", err)
 		db.Close()
 		return nil, err
 	}
-	log.Println("[InitDB] Database connection established successfully.")
+
+	log.Println("[InitDB] Database connection established.")
 	return db, nil
 }
