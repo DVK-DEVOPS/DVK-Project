4616fcc8bce9127792785264355fea4cacdbbf96
ViAarisKea <viaa0001@stud.ek.dk>
2025-11-12 12:51:17 +0100

    remove reading schema every time

--- go/db/db.go
+++ go/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"DVK-Project/config"
 	"database/sql"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -11,23 +12,19 @@ import (
 
 func InitDB() (*sql.DB, error) {
 
+	log.Println("[InitDB] Loading environment variables...")
 	_ = godotenv.Load()
+
+	log.Println("[InitDB] Getting database URL secret...")
 	dbURL := config.GetSecret("DB_URL", os.Getenv("AZURE_KEYVAULT_NAME"), os.Getenv("DB_URL_SECRET_NAME"))
 
+	log.Println("[InitDB] Opening database connection...")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
+		log.Printf("[InitDB] Error opening database connection: %v\n", err)
 		return nil, err
 	}
 
-	sql, err := os.ReadFile("/whoknows/go/schema.sql")
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = db.Exec(string(sql))
-	if err != nil {
-		return nil, err
-	}
-
+	log.Println("[InitDB] Database connection established successfully.")
 	return db, nil
 }
