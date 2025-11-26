da6e090f492ed2d4d7635ae38ff8e005e2b39e49
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-11-26 11:40:39 +0100

    basuerl cahnged

--- go/test/e2e/tests/app.spec.ts
+++ go/test/e2e/tests/app.spec.ts
@@ -1,44 +1,35 @@
 import { expect, test } from "@playwright/test";
 
-const BASE_URL = "http://whoknows:8080";
-
 test("homepage loads", async ({ page }) => {
-  await page.goto(BASE_URL);
+  await page.goto("/");
   await expect(page.getByRole("textbox", { name: "Search..." })).toBeVisible();
 });
 
 test("search works", async ({ page }) => {
-  await page.goto(BASE_URL);
-
+  await page.goto("/");
   await page.getByRole("textbox", { name: "Search..." }).fill("java");
   await page.getByRole("button", { name: "Search" }).click();
-
   await expect(
     page.getByText("Java Streams Examples v2 progurl2.dev-v2")
   ).toBeVisible();
 });
 
 test("navigation works", async ({ page }) => {
-  await page.goto(BASE_URL);
-
+  await page.goto("/");
   await page.getByRole("link", { name: "Register" }).click();
   await expect(page).toHaveURL(/register/);
-
   await page.getByRole("link", { name: "Already have an account?" }).click();
   await expect(page).toHaveURL(/login/);
 });
 
 test("register validation", async ({ page }) => {
-  await page.goto(`${BASE_URL}/register`);
-
+  await page.goto("/register");
   await page.getByRole("button", { name: "Sign up" }).click();
-
   await expect(page.getByText('{"detail":[{"loc":["body","')).toBeVisible();
 });
 
 test("login flow", async ({ page }) => {
-  await page.goto(`${BASE_URL}/login`);
-
+  await page.goto("/login");
   await page.locator('input[name="username"]').fill("test");
   await page.locator('input[name="password"]').fill("test");
   await page.getByRole("button", { name: "Log in" }).click();
